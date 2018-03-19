f9fd0331f9eb847c9f4d4bed2fc4e2fa572e083e
Daniel Doyle <dbdoyle182@gmail.com>
2018-03-19 17:01:28 -0400

    connected liri.js added spotify keys able to log my specific keys

--- liri.js
+++ liri.js
@@ -1,3 +1,9 @@
 require("dotenv").config();
+var fs = require("fs")
+var keys = require("./keys.js");
 
-var apiKey = require("./keys.js")
+var twitter = require("twitter");
+var spotify = require("spotify");
+
+var client = new twitter(keys.twitterKeys);
+var spotify = new spotify(keys.spotifyKeys);
